30a8371a924cb5997d5b2a9b4c41237e5ef3e4d3
ashan_manuka <manukajayarathne.coma@gmail.com>
2024-03-02 09:26:04 +0530

    updated styles

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -26,7 +26,7 @@ function Dashboard() {
 
     return(
         <div id="mainDiv">
-            <div className="subDiv">
+            <div className="mainSubDiv">
                 <h2>Enjoy Your</h2><h1>Dashboard</h1>
 
                 <h3 className='simpleTxt'>Aralaganvila {branchName}</h3>
@@ -36,22 +36,22 @@ function Dashboard() {
             <div id='subDivv'>
                 <div class="grid-containerr">
                     <div class="grid-itemm">
-                        <button className='dashboardBtn' id='btnOne' onClick={() => navigateFromDashboard('one')}><h1>New Loan</h1></button>
+                        <button className='dashboardBtn' id='btnOne' onClick={() => navigateFromDashboard('one')}><h1>නව බාණ්ඩයක් උකස් තැබීම</h1></button>
                     </div>
                     <div class="grid-itemm">
-                        <button className='dashboardBtn' id='btnOne' onClick={() => navigateFromDashboard('two')}><h1>Make a Payment</h1></button>
+                        <button className='dashboardBtn' id='btnOne' onClick={() => navigateFromDashboard('two')}><h1>බාණ්ඩයක් බේරා ගැනීමට</h1></button>
                     </div>
                     <div class="grid-itemm">
-                        <button className='dashboardBtn' id='btnOne' onClick={() => navigateFromDashboard('three')}><h1>Payment of Interest</h1></button>
+                        <button className='dashboardBtn' id='btnOne' onClick={() => navigateFromDashboard('three')}><h1>පොලිය ගෙවීම</h1></button>
                     </div>
                     <div class="grid-itemm">
-                    <button className='dashboardBtn' id='btnOne' onClick={() => navigateFromDashboard('four')}><h1>Renewal of loan</h1></button>
+                    <button className='dashboardBtn' id='btnOne' onClick={() => navigateFromDashboard('four')}><h1>ණය අලුත් කිරීම</h1></button>
                     </div>
                     <div class="grid-itemm">
-                        <button className='dashboardBtn' id='btnOne' onClick={() => navigateFromDashboard('five')}><h1>Loan Details</h1></button>
+                        <button className='dashboardBtn' id='btnOne' onClick={() => navigateFromDashboard('five')}><h1>සියලු රන් ණය විස්තර</h1></button>
                     </div>
                     <div class="grid-itemm">
-                    <button className='dashboardBtn' id='btnOne' onClick={() => navigateFromDashboard('six')}><h1>Customer Details</h1></button>
+                    <button className='dashboardBtn' id='btnOne' onClick={() => navigateFromDashboard('six')}><h1>සියලු පාරිබෝගික විස්තර</h1></button>
                     </div>
                 </div>
             </div>
--- src/component/MakeLoan.js
+++ src/component/MakeLoan.js
@@ -7,6 +7,7 @@ function MakeLoan() {
     const [customerNic, setCustomerNic] = useState("");
     const [customerName, setCustomerName] = useState("");
     const [customerAddress, setCustomerAddress] = useState("");
+    const [itemDescription, setItemDescription] = useState("");
     const [customerTelephone, setCustomerTelephone] = useState("");
     const [loanNumber, setLoanNumber] = useState("");
     const [totalWeight, setTotalWeight] = useState("");
@@ -14,6 +15,8 @@ function MakeLoan() {
     const [estimatedValue, setEstimatedValue] = useState("");
     const [marketValue, setMarketValue] = useState("");
     const [LoanAmount, setLoanAmount] = useState("");
+    const [LoanDuration, setLoanDuration] = useState("");
+    const [LoanInterest, setLoanInterest] = useState("");
 
     const changeCustomerNic = (nameInput) => {
         setCustomerNic(nameInput);
@@ -26,6 +29,9 @@ function MakeLoan() {
     }
     const changeCustomerTelephone = (nameInput) => {
         setCustomerTelephone(nameInput);
+    }
+    const changeItemDescription = (nameInput) => {
+        setItemDescription(nameInput);
     }
     const changeLoanNumber = (nameInput) => {
         setLoanNumber(nameInput);
@@ -45,6 +51,12 @@ function MakeLoan() {
     const changeLoanAmount = (nameInput) => {
         setLoanAmount(nameInput);
     }
+    const changeLoanDuration = (nameInput) => {
+        setLoanDuration(nameInput);
+    }
+    const changeLoanInterest = (nameInput) => {
+        setLoanInterest(nameInput);
+    }
     
 
     const confirmLoan = () => {
@@ -53,16 +65,17 @@ function MakeLoan() {
 
     return(
         <div id="mainDiv">
-            <div className="subDiv">
+            <div className="loanSubDiv">
                 <h1>Gold Loan</h1><h2>Dashboard</h2>
             </div>
 
             <div id='userDetailCard'>
-                <h3>Customer's Details,</h3>
-                <input id='inputOne' className='textInput' placeholder=' Nic' onChange={changeCustomerNic}></input>
-                <input id='inputTwo' className='textInput' placeholder=' Name' onChange={changeCustomerName}></input>
-                <input id='inputThree' className='textInput' placeholder=' Address' onChange={changeCustomerAddress}></input>
-                <input id='inputFour' className='textInput' placeholder=' Telephone' onChange={changeCustomerTelephone}></input>
+                <h3>ණය ලබන්නා පිළිබද විස්තර,</h3>
+                <input id='inputOne' className='textInput' placeholder=' ජාතික හැදුනුම්පත් අංකය' onChange={changeCustomerNic}></input>
+                <input id='inputTwo' className='textInput' placeholder=' සම්පුර්ණ නම' onChange={changeCustomerName}></input>
+                <input id='inputThree' className='textInput' placeholder=' ලිපිනය' onChange={changeCustomerAddress}></input>
+                <input id='inputFour' className='textInput' placeholder=' දුරකථන අංකය' onChange={changeCustomerTelephone}></input>
+                <input id='inputFive' className='textInput' placeholder=' රන් බාණ්ඩයේ වර්ගය සහ විස්තරය' onChange={changeItemDescription}></input>
             </div>
 
             <div id='itemDetailCard'>
@@ -78,9 +91,13 @@ function MakeLoan() {
                 <input id='itemInputFive' className='textInputTwo' onChange={changeMarketValue}></input>
                 <h3>ණය මුදල</h3>
                 <input id='itemInputSix' className='textInputTwo' onChange={changeLoanAmount}></input>
+                <h3>ගෙවා අවසන් කිරීම</h3>
+                <input id='itemInputSeven' className='textInputTwo' onChange={changeLoanDuration}></input>
+                <h3>පොලිය</h3>
+                <input id='itemInputEight' className='textInputTwo' onChange={changeLoanInterest}></input>
             </div>
 
-            <button id='confirmBtn' onClick={confirmLoan}>Confirm Loan</button>
+            <button id='confirmBtn' onClick={confirmLoan}>Confirm Loan & Print Bill</button>
 
 
 
